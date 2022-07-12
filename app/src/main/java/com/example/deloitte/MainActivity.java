eb6db6ed808999320ffbe3db78c168f72f003c51
abdul ansari <abdul.tanveer@gmail.com>
2022-07-12 11:42:58 +0530

    get data from edittext and set into textview

--- app/src/main/java/com/example/deloitte/MainActivity.java
+++ app/src/main/java/com/example/deloitte/MainActivity.java
@@ -5,20 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a; //declaration
+    Student deloiteStudent; //declaration
+    EditText nameEditText; //declaration
+    TextView mainTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+          a = 5; //initialization
+        setContentView(R.layout.activity_main); //inflating --layout inflater
+        nameEditText = findViewById(R.id.etName); //initialization -- taking handle
+        mainTextView = findViewById(R.id.tvMain);
+        deloiteStudent = new Student(); //instantiation
+        deloiteStudent.setName("yatish");
+
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "logging in", Toast.LENGTH_SHORT).show();
-        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
-        startActivity(hIntent);
+        //get the text from edittext
+        String name = nameEditText.getText().toString();
+        //set the text into the textview
+        mainTextView.setText(name);
+        // Toast.makeText(this, "logging in", Toast.LENGTH_SHORT).show();
+        /*Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(hIntent);*/
     }
 
     void drawRectangle(int length, int breadth){}
--- app/src/main/java/com/example/deloitte/Student.java
+++ app/src/main/java/com/example/deloitte/Student.java
@@ -1,5 +1,6 @@
 package com.example.deloitte;
 
+//user defined datatypes
 public class Student {
 
     int rollno;
@@ -8,4 +9,36 @@ public class Student {
     String address;
 
     int calculateStipend(){ return 100;}
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhno() {
+        return phno;
+    }
+
+    public void setPhno(int phno) {
+        this.phno = phno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
