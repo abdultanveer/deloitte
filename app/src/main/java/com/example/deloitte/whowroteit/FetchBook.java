93e75c83624ba9a1bfc3c0e67d12aa34ec76b3e6
abdul ansari <abdul.tanveer@gmail.com>
2022-07-20 14:21:43 +0530

    whowroteit-2

--- app/src/main/java/com/example/deloitte/whowroteit/FetchBook.java
+++ app/src/main/java/com/example/deloitte/whowroteit/FetchBook.java
@@ -3,6 +3,10 @@ package com.example.deloitte.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
@@ -23,6 +27,40 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String jsonString) {
         super.onPostExecute(jsonString);
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+            if (title != null && authors != null) {
+                mTitleText.get().setText(title);
+                mAuthorText.get().setText(authors);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
