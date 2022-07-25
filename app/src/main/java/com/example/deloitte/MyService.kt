ca0a5fbac0be086dec7f36f8184de20bc42182d5
abdul ansari <abdul.tanveer@gmail.com>
2022-07-25 14:56:29 +0530

    bound service

--- app/src/main/java/com/example/deloitte/MyService.kt
+++ app/src/main/java/com/example/deloitte/MyService.kt
@@ -2,10 +2,12 @@ package com.example.deloitte
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
+    private val binder = LocalBinder()
 
     override fun onCreate() {
         super.onCreate()
@@ -17,12 +19,14 @@ class MyService : Service() {
         Log.i(TAG,"service started--"+intent?.getStringExtra("musicfilename"))
        // stopSelf()
         return START_STICKY
+    }
 
-
+    fun add(s1: Int,s2:Int):Int {
+        return s1 + s2
     }
 
     override fun onBind(intent: Intent): IBinder? {
-        return null
+        return binder //binder as a pipe
     }
 
     override fun onDestroy() {
@@ -35,5 +39,17 @@ class MyService : Service() {
     companion object{
         var TAG = MyService::class.java.simpleName
     }
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyService = this@MyService
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/deloitte/ServiceActivity.kt
+++ app/src/main/java/com/example/deloitte/ServiceActivity.kt
@@ -1,9 +1,14 @@
 package com.example.deloitte
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import android.widget.Toast
+import java.util.*
 
 class ServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +27,27 @@ class ServiceActivity : AppCompatActivity() {
                 var serviceIntent = Intent(this,MyService::class.java)
                 stopService(serviceIntent)
             }
+            R.id.btnBind -> {
+                var serviceIntent = Intent(this,MyService::class.java)
+                bindService(serviceIntent,serviceConnection, BIND_AUTO_CREATE)
+            }
         }
+    }
+
+    private val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(connctionName: ComponentName?, binder: IBinder?) {
+        //binder returned by bind method of the service
+            val binder = binder as MyService.LocalBinder
+           var myService =  binder.getService()  //var myService = new Myservice()
+            //i'll play with myService object as it was my local variab;e
+           var result = myService.add(10,20);
+            Toast.makeText(applicationContext,"the result is"+result,Toast.LENGTH_SHORT).show()
+
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
