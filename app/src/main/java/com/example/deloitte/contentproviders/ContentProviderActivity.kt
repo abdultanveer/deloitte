c69bd171b4707261301f08bda74b5d6ef1bbdef9
abdul ansari <abdul.tanveer@gmail.com>
2022-07-26 11:56:05 +0530

    content providers- runtime permissions

--- app/src/main/java/com/example/deloitte/contentproviders/ContentProviderActivity.kt
+++ app/src/main/java/com/example/deloitte/contentproviders/ContentProviderActivity.kt
@@ -5,27 +5,46 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
-import androidx.appcompat.app.AppCompatActivity
 import com.example.deloitte.R
 
-class ContentProviderActivity : AppCompatActivity() {
+class ContentProviderActivity : BasePermissionAppCompatActivity() {
     lateinit var cpListView: ListView
+    lateinit var dataCursor: Cursor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_provider)
         cpListView = findViewById(R.id.cpListview)
         val tableNameUri: Uri = Uri.parse("content://sms/inbox")
+        getReadSMSPermission(object : RequestPermissionAction {
+            override fun permissionDenied() {
+                // Call Back, when permission is Denied
+            }
 
-       var dataCursor: Cursor? = contentResolver.query(tableNameUri,null,null,null,null)
+            override fun permissionGranted() {
+                // Call Back, when permission is Granted
+                 dataCursor =
+                     contentResolver.query(tableNameUri, null, null, null, null)!!
+                var fromColNames = arrayOf("body", "address")
+                var toTextviewIds = intArrayOf(android.R.id.text1, android.R.id.text2)
 
-        var fromColNames = arrayOf("body","address")
-        var toTextviewIds = intArrayOf(android.R.id.text1,android.R.id.text2)
+                var myAdapter = SimpleCursorAdapter(
+                    applicationContext,
+                    android.R.layout.simple_list_item_2,
+                    dataCursor,
+                    fromColNames,
+                    toTextviewIds
+                )
+                cpListView.adapter = myAdapter
 
-        var myAdapter = SimpleCursorAdapter(this,
-            android.R.layout.simple_list_item_2,
-            dataCursor,
-            fromColNames,
-            toTextviewIds)
-        cpListView.adapter = myAdapter
+            }
+
+        });
+
+
     }
+
+
+
+
+
 }
