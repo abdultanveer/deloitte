3833bfca291b48d55b55884cf8f16e10242b37e9
abdul ansari <abdul.tanveer@gmail.com>
2022-07-27 14:21:18 +0530

    fragments

---
+++ app/src/main/java/com/example/deloitte/fragments/HeadlinesFragment.java
@@ -0,0 +1,23 @@
+package com.example.deloitte.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.deloitte.R;
+
+public class HeadlinesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+         View view = inflater.inflate(R.layout.fragment_headlines,container,false);
+        return view;
+    }
+}
